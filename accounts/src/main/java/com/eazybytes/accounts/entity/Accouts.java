57c437613e3a2f30a9ab61612ff21ea3a141ebdc
FelipeGabrill <felipegabrielodss@gmail.com>
2025-02-24 03:33:08 -0300

    Writing Spring Data JPA entities & repositories to interact with DB tables

---
+++ accounts/src/main/java/com/eazybytes/accounts/entity/Accouts.java
@@ -0,0 +1,25 @@
+package com.eazybytes.accounts.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter @Setter @ToString @AllArgsConstructor @NoArgsConstructor
+public class Accouts extends BaseEntity {
+
+    @Column(name = "account_number")
+    @Id
+    private Long accountNumber;
+
+    @Column(name = "customer_id")
+    private Long customerId;
+
+    @Column(name = "account_type")
+    private String accountType;
+
+    @Column(name = "branch_address")
+    private String branchAddress;
+
+    @Column(name = "mobile_number")
+    private String mobileNumber;
+}
